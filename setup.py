2860eeafdfb4da1d58de587c06d71b56cb956de4
Rohit <guliajat00@gmail.com>
2026-01-04 09:47:59 +0530

    data folder is added to dvc for tracking

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='complete pipeline of data science using cookicutter and dvc',
+    author='rohit',
+    license='',
+)
